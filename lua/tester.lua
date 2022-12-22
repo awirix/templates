c66e8de1aa3c99b96f996c5fcaf6bd14e1db9136
metafates <fates@duck.com>
2022-12-22 09:52:30 +0300

    refactor: improve structure

--- lua/tester.lua
+++ lua/tester.lua
@@ -1,5 +1,3 @@
-{{- /*gotype:github.com/vivi-app/vivi/template.meta*/ -}}-- vim:ts=3 ss=3 sw=3 expandtab
-
 local M = {}
 
 --- Tests this extension.
--- preset.go
+++ preset.go
@@ -4,6 +4,8 @@ import (
 	"bytes"
 	"fmt"
 	"io/fs"
+	"path/filepath"
+	"strings"
 	"text/template"
 )
 
@@ -13,7 +15,7 @@ type Preset int
 
 const (
 	// PresetLua is a preset for Lua
-	PresetLua Preset = iota
+	PresetLua Preset = iota + 1
 
 	// PresetFennel is a preset for Fennel
 	PresetFennel
@@ -22,25 +24,28 @@ const (
 	PresetTypescript
 )
 
+// ErrInvalidPreset is returned when an invalid preset is given
 type ErrInvalidPreset error
 
+// Get gets the template tree for the given preset.
+// Returned map is a map of file names to file contents
 func Get(preset Preset, info Info) (map[string]*bytes.Buffer, error) {
 	var f fs.FS
 
 	switch preset {
 	case PresetLua:
-		f = FSLua
+		f = fsLua
 	case PresetFennel:
-		f = FSFennel
+		f = fsFennel
 	case PresetTypescript:
-		f = FSTypescript
+		f = fsTypescript
 	default:
 		return nil, ErrInvalidPreset(fmt.Errorf("invalid preset: %s", preset))
 	}
 
 	var tree = map[string]*bytes.Buffer{
-		".gitignore":    bytes.NewBuffer(GitIgnore),
-		".editorconfig": bytes.NewBuffer(EditorConfig),
+		".gitignore":    bytes.NewBuffer(gitIgnore),
+		".editorconfig": bytes.NewBuffer(editorConfig),
 	}
 
 	err := fs.WalkDir(f, ".", func(path string, d fs.DirEntry, err error) error {
@@ -57,20 +62,31 @@ func Get(preset Preset, info Info) (map[string]*bytes.Buffer, error) {
 			return err
 		}
 
-		t := template.New(preset.String())
-		t, err = t.Parse(string(b))
-		if err != nil {
-			return err
+		var (
+			buf  bytes.Buffer
+			name string
+		)
+
+		// Execute template only if it's a .tmpl file
+		if filepath.Ext(d.Name()) == ".tmpl" {
+			name = strings.TrimSuffix(d.Name(), ".tmpl")
+
+			t := template.New(preset.String())
+			t, err = t.Parse(string(b))
+			if err != nil {
+				return err
+			}
+
+			err = t.Execute(&buf, info)
+			if err != nil {
+				return err
+			}
+		} else {
+			name = d.Name()
+			buf.Write(b)
 		}
 
-		var buf bytes.Buffer
-
-		err = t.Execute(&buf, info)
-		if err != nil {
-			return err
-		}
-
-		tree[d.Name()] = &buf
+		tree[name] = &buf
 		return nil
 	})
 
